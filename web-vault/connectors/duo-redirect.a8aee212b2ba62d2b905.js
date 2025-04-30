a6df9d52cfb676e2f7b6d5e77d0212941da8d734
lukagocharov <newtonvlema@gmail.com>
2025-04-30 18:07:53 +0300

    apr30-2

--- web-vault/connectors/duo-redirect.a8aee212b2ba62d2b905.js
+++ web-vault/connectors/duo-redirect.a8aee212b2ba62d2b905.js
@@ -1,2 +1,278 @@
-(()=>{"use strict";var e={77321:(e,t,n)=>{n.r(t)}},t={};function n(o){var s=t[o];if(void 0!==s)return s.exports;var a=t[o]={exports:{}};return e[o](a,a.exports,n),a.exports}function o(e){const t=window.location.href;e=e.replace(/[\[\]]/g,"\\$&");const n=new RegExp("[?&]"+e+"(=([^&#]*)|&|#|$)").exec(t);return n?n[2]?decodeURIComponent(n[2].replace(/\+/g," ")):"":null}n.r=e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})};var s=function(e,t,n,o){return new(n||(n=Promise))((function(s,a){function i(e){try{r(o.next(e))}catch(t){a(t)}}function l(e){try{r(o.throw(e))}catch(t){a(t)}}function r(e){var t;e.done?s(e.value):(t=e.value,t instanceof n?t:new n((function(e){e(t)}))).then(i,l)}r((o=o.apply(e,t||[])).next())}))};class a{constructor(e,t,n){this.systemLanguage=e,this.localesDirectory=t,this.getLocalesJson=n,this.supportedTranslationLocales=["en"],this.defaultLocale="en",this.localeNames=new Map([["af","Afrikaans"],["ar","العربية الفصحى"],["az","Azərbaycanca"],["be","Беларуская"],["bg","български"],["bn","বাংলা"],["bs","bosanski jezik"],["ca","català"],["cs","čeština"],["cy","Cymraeg, y Gymraeg"],["da","dansk"],["de","Deutsch"],["el","Ελληνικά"],["en","English"],["en-GB","English (British)"],["en-IN","English (India)"],["eo","Esperanto"],["es","español"],["et","eesti"],["eu","euskara"],["fa","فارسی"],["fi","suomi"],["fil","Wikang Filipino"],["fr","français"],["gl","galego"],["he","עברית"],["hi","हिन्दी"],["hr","hrvatski"],["hu","magyar"],["id","Bahasa Indonesia"],["it","italiano"],["ja","日本語"],["ka","ქართული"],["km","ខ្មែរ, ខេមរភាសា, ភាសាខ្មែរ"],["kn","ಕನ್ನಡ"],["ko","한국어"],["lt","lietuvių kalba"],["lv","Latvietis"],["me","црногорски"],["ml","മലയാളം"],["mr","मराठी"],["my","ဗမာစကား"],["nb","norsk (bokmål)"],["ne","नेपाली"],["nl","Nederlands"],["nn","Norsk Nynorsk"],["or","ଓଡ଼ିଆ"],["pl","polski"],["pt-BR","português do Brasil"],["pt-PT","português"],["ro","română"],["ru","русский"],["si","සිංහල"],["sk","slovenčina"],["sl","Slovenski jezik, Slovenščina"],["sr","Српски"],["sv","svenska"],["te","తెలుగు"],["th","ไทย"],["tr","Türkçe"],["uk","українська"],["vi","Tiếng Việt"],["zh-CN","中文（中国大陆）"],["zh-TW","中文（台灣）"]]),this.defaultMessages={},this.localeMessages={},this.systemLanguage=e.replace("_","-")}init(e){return s(this,void 0,void 0,(function*(){if(this.inited)throw new Error("i18n already initialized.");if(null==this.supportedTranslationLocales||0===this.supportedTranslationLocales.length)throw new Error("supportedTranslationLocales not set.");this.inited=!0,this.translationLocale=null!=e?e:this.systemLanguage;try{this.collator=new Intl.Collator(this.translationLocale,{numeric:!0,sensitivity:"base"})}catch(t){this.collator=null}-1===this.supportedTranslationLocales.indexOf(this.translationLocale)&&(this.translationLocale=this.translationLocale.slice(0,2),-1===this.supportedTranslationLocales.indexOf(this.translationLocale)&&(this.translationLocale=this.defaultLocale)),null!=this.localesDirectory&&(yield this.loadMessages(this.translationLocale,this.localeMessages),this.translationLocale!==this.defaultLocale&&(yield this.loadMessages(this.defaultLocale,this.defaultMessages)))}))}t(e,t,n,o){return this.translate(e,t,n,o)}translate(e,t,n,o){let s;return s=this.localeMessages.hasOwnProperty(e)&&this.localeMessages[e]?this.localeMessages[e]:this.defaultMessages.hasOwnProperty(e)&&this.defaultMessages[e]?this.defaultMessages[e]:"",""!==s&&(null!=t&&(s=s.split("__$1__").join(t.toString())),null!=n&&(s=s.split("__$2__").join(n.toString())),null!=o&&(s=s.split("__$3__").join(o.toString()))),s}loadMessages(e,t){return s(this,void 0,void 0,(function*(){const n=e.replace("-","_"),o=yield this.getLocalesJson(n);for(const e in o)if(o.hasOwnProperty(e)&&(t[e]=o[e].message,o[e].placeholders))for(const n in o[e].placeholders){if(!o[e].placeholders.hasOwnProperty(n)||!o[e].placeholders[n].content)continue;const s="\\$"+n.toUpperCase()+"\\$";let a=o[e].placeholders[n].content;"$1"!==a&&"$2"!==a&&"$3"!==a||(a="__$"+a+"__"),t[e]=t[e].replace(new RegExp(s,"g"),a)}}))}}const i=["en","af","ar","az","be","bg","bn","bs","ca","cs","cy","da","de","el","en-GB","en-IN","eo","es","et","eu","fa","fi","fil","fr","gl","he","hi","hr","hu","id","it","ja","ka","km","kn","ko","lv","ml","mr","my","nb","ne","nl","nn","or","pl","pt-PT","pt-BR","ro","ru","si","sk","sl","sr","sv","te","th","tr","uk","vi","zh-CN","zh-TW"];var l=function(e,t,n,o){return new(n||(n=Promise))((function(s,a){function i(e){try{r(o.next(e))}catch(t){a(t)}}function l(e){try{r(o.throw(e))}catch(t){a(t)}}function r(e){var t;e.done?s(e.value):(t=e.value,t instanceof n?t:new n((function(e){e(t)}))).then(i,l)}r((o=o.apply(e,t||[])).next())}))};class r extends a{constructor(e,t){super(e||"en-US",t,(e=>l(this,void 0,void 0,(function*(){const t=this.localesDirectory+"/"+e+"/messages.json?cache=8ikt5mn",n=yield fetch(t);return yield n.json()})))),this.supportedTranslationLocales=i}get translationLocale(){return this._translationLocale}set translationLocale(e){this._translationLocale=e}}var c=function(e,t,n,o){return new(n||(n=Promise))((function(s,a){function i(e){try{r(o.next(e))}catch(t){a(t)}}function l(e){try{r(o.throw(e))}catch(t){a(t)}}function r(e){var t;e.done?s(e.value):(t=e.value,t instanceof n?t:new n((function(e){e(t)}))).then(i,l)}r((o=o.apply(e,t||[])).next())}))};n(77321);let d=null;function u(e){const t=document.getElementById("content");t.className="text-center",t.innerHTML="";const n=document.createElement("h1"),o=document.createElement("p");if(n.textContent=d.t("youSuccessfullyLoggedIn"),o.textContent="web"==e?o.textContent=d.t("thisWindowWillCloseIn5Seconds"):d.t("youMayCloseThisWindow"),n.className="font-weight-semibold",o.className="mb-4",t.appendChild(n),t.appendChild(o),"web"==e){const e=document.createElement("button");e.textContent=d.t("close"),e.className="bg-primary text-white border-0 rounded py-2 px-3",e.addEventListener("click",(()=>{window.close()})),t.appendChild(e);let n=Number(o.textContent.match(/\d+/)[0]);const s=setInterval((()=>{n>1?(o.textContent=o.textContent.replace(String(n),String(n-1)),n--):(clearInterval(s),window.close())}),1e3)}}window.addEventListener("load",(()=>c(void 0,void 0,void 0,(function*(){const e=o("duoFramelessUrl");if(e)return void function(e){const t=new URL(e);if("https:"!==t.protocol||!t.hostname.endsWith(".duosecurity.com")&&!t.hostname.endsWith(".duofederal.com"))throw new Error("Invalid redirect URL");window.location.href=decodeURIComponent(e)}(e);const t=o("client"),n=o("code"),s=o("state");if(d=new r(navigator.language,"locales"),yield d.init(),"web"===t){const e=new BroadcastChannel("duoResult");e.postMessage({code:n,state:s}),e.close(),u(t)}else"browser"===t?(window.postMessage({command:"duoResult",code:n,state:s},"*"),u(t)):"mobile"!==t&&"desktop"!==t||("desktop"===t&&u(t),document.location.replace("bitwarden://duo-callback?code="+encodeURIComponent(n)+"&state="+encodeURIComponent(s)))}))))})();
+(() => {
+	"use strict";
+	var e = {
+			77321: (e, t, n) => {
+				n.r(t)
+			}
+		},
+		t = {};
+
+	function n(o) {
+		var s = t[o];
+		if (void 0 !== s) return s.exports;
+		var a = t[o] = {
+			exports: {}
+		};
+		return e[o](a, a.exports, n), a.exports
+	}
+
+	function o(e) {
+		const t = window.location.href;
+		e = e.replace(/[\[\]]/g, "\\$&");
+		const n = new RegExp("[?&]" + e + "(=([^&#]*)|&|#|$)").exec(t);
+		return n ? n[2] ? decodeURIComponent(n[2].replace(/\+/g, " ")) : "" : null
+	}
+	n.r = e => {
+		"undefined" != typeof Symbol && Symbol.toStringTag && Object.defineProperty(e, Symbol.toStringTag, {
+			value: "Module"
+		}), Object.defineProperty(e, "__esModule", {
+			value: !0
+		})
+	};
+	var s = function(e, t, n, o) {
+		return new(n || (n = Promise))((function(s, a) {
+			function i(e) {
+				try {
+					r(o.next(e))
+				} catch (t) {
+					a(t)
+				}
+			}
+
+			function l(e) {
+				try {
+					r(o.throw(e))
+				} catch (t) {
+					a(t)
+				}
+			}
+
+			function r(e) {
+				var t;
+				e.done ? s(e.value) : (t = e.value, t instanceof n ? t : new n((function(e) {
+					e(t)
+				}))).then(i, l)
+			}
+			r((o = o.apply(e, t || [])).next())
+		}))
+	};
+	class a {
+		constructor(e, t, n) {
+			this.systemLanguage = e, this.localesDirectory = t, this.getLocalesJson = n, this.supportedTranslationLocales = ["en"], this.defaultLocale = "en", this.localeNames = new Map([
+				["af", "Afrikaans"],
+				["ar", "العربية الفصحى"],
+				["az", "Azərbaycanca"],
+				["be", "Беларуская"],
+				["bg", "български"],
+				["bn", "বাংলা"],
+				["bs", "bosanski jezik"],
+				["ca", "català"],
+				["cs", "čeština"],
+				["cy", "Cymraeg, y Gymraeg"],
+				["da", "dansk"],
+				["de", "Deutsch"],
+				["el", "Ελληνικά"],
+				["en", "English"],
+				["en-GB", "English (British)"],
+				["en-IN", "English (India)"],
+				["eo", "Esperanto"],
+				["es", "español"],
+				["et", "eesti"],
+				["eu", "euskara"],
+				["fa", "فارسی"],
+				["fi", "suomi"],
+				["fil", "Wikang Filipino"],
+				["fr", "français"],
+				["gl", "galego"],
+				["he", "עברית"],
+				["hi", "हिन्दी"],
+				["hr", "hrvatski"],
+				["hu", "magyar"],
+				["id", "Bahasa Indonesia"],
+				["it", "italiano"],
+				["ja", "日本語"],
+				["ka", "ქართული"],
+				["km", "ខ្មែរ, ខេមរភាសា, ភាសាខ្មែរ"],
+				["kn", "ಕನ್ನಡ"],
+				["ko", "한국어"],
+				["lt", "lietuvių kalba"],
+				["lv", "Latvietis"],
+				["me", "црногорски"],
+				["ml", "മലയാളം"],
+				["mr", "मराठी"],
+				["my", "ဗမာစကား"],
+				["nb", "norsk (bokmål)"],
+				["ne", "नेपाली"],
+				["nl", "Nederlands"],
+				["nn", "Norsk Nynorsk"],
+				["or", "ଓଡ଼ିଆ"],
+				["pl", "polski"],
+				["pt-BR", "português do Brasil"],
+				["pt-PT", "português"],
+				["ro", "română"],
+				["ru", "русский"],
+				["si", "සිංහල"],
+				["sk", "slovenčina"],
+				["sl", "Slovenski jezik, Slovenščina"],
+				["sr", "Српски"],
+				["sv", "svenska"],
+				["te", "తెలుగు"],
+				["th", "ไทย"],
+				["tr", "Türkçe"],
+				["uk", "українська"],
+				["vi", "Tiếng Việt"],
+				["zh-CN", "中文（中国大陆）"],
+				["zh-TW", "中文（台灣）"]
+			]), this.defaultMessages = {}, this.localeMessages = {}, this.systemLanguage = e.replace("_", "-")
+		}
+		init(e) {
+			return s(this, void 0, void 0, (function*() {
+				if (this.inited) throw new Error("i18n already initialized.");
+				if (null == this.supportedTranslationLocales || 0 === this.supportedTranslationLocales.length) throw new Error("supportedTranslationLocales not set.");
+				this.inited = !0, this.translationLocale = null != e ? e : this.systemLanguage;
+				try {
+					this.collator = new Intl.Collator(this.translationLocale, {
+						numeric: !0,
+						sensitivity: "base"
+					})
+				} catch (t) {
+					this.collator = null
+				} - 1 === this.supportedTranslationLocales.indexOf(this.translationLocale) && (this.translationLocale = this.translationLocale.slice(0, 2), -1 === this.supportedTranslationLocales.indexOf(this.translationLocale) && (this.translationLocale = this.defaultLocale)), null != this.localesDirectory && (yield this.loadMessages(this.translationLocale, this.localeMessages), this.translationLocale !== this.defaultLocale && (yield this.loadMessages(this.defaultLocale, this.defaultMessages)))
+			}))
+		}
+		t(e, t, n, o) {
+			return this.translate(e, t, n, o)
+		}
+		translate(e, t, n, o) {
+			let s;
+			return s = this.localeMessages.hasOwnProperty(e) && this.localeMessages[e] ? this.localeMessages[e] : this.defaultMessages.hasOwnProperty(e) && this.defaultMessages[e] ? this.defaultMessages[e] : "", "" !== s && (null != t && (s = s.split("__$1__").join(t.toString())), null != n && (s = s.split("__$2__").join(n.toString())), null != o && (s = s.split("__$3__").join(o.toString()))), s
+		}
+		loadMessages(e, t) {
+			return s(this, void 0, void 0, (function*() {
+				const n = e.replace("-", "_"),
+					o = yield this.getLocalesJson(n);
+				for (const e in o)
+					if (o.hasOwnProperty(e) && (t[e] = o[e].message, o[e].placeholders))
+						for (const n in o[e].placeholders) {
+							if (!o[e].placeholders.hasOwnProperty(n) || !o[e].placeholders[n].content) continue;
+							const s = "\\$" + n.toUpperCase() + "\\$";
+							let a = o[e].placeholders[n].content;
+							"$1" !== a && "$2" !== a && "$3" !== a || (a = "__$" + a + "__"), t[e] = t[e].replace(new RegExp(s, "g"), a)
+						}
+			}))
+		}
+	}
+	const i = ["en", "af", "ar", "az", "be", "bg", "bn", "bs", "ca", "cs", "cy", "da", "de", "el", "en-GB", "en-IN", "eo", "es", "et", "eu", "fa", "fi", "fil", "fr", "gl", "he", "hi", "hr", "hu", "id", "it", "ja", "ka", "km", "kn", "ko", "lv", "ml", "mr", "my", "nb", "ne", "nl", "nn", "or", "pl", "pt-PT", "pt-BR", "ro", "ru", "si", "sk", "sl", "sr", "sv", "te", "th", "tr", "uk", "vi", "zh-CN", "zh-TW"];
+	var l = function(e, t, n, o) {
+		return new(n || (n = Promise))((function(s, a) {
+			function i(e) {
+				try {
+					r(o.next(e))
+				} catch (t) {
+					a(t)
+				}
+			}
+
+			function l(e) {
+				try {
+					r(o.throw(e))
+				} catch (t) {
+					a(t)
+				}
+			}
+
+			function r(e) {
+				var t;
+				e.done ? s(e.value) : (t = e.value, t instanceof n ? t : new n((function(e) {
+					e(t)
+				}))).then(i, l)
+			}
+			r((o = o.apply(e, t || [])).next())
+		}))
+	};
+	class r extends a {
+		constructor(e, t) {
+			super(e || "en-US", t, (e => l(this, void 0, void 0, (function*() {
+				const t = this.localesDirectory + "/" + e + "/messages.json?cache=8ikt5mn",
+					n = yield fetch(t);
+				return yield n.json()
+			})))), this.supportedTranslationLocales = i
+		}
+		get translationLocale() {
+			return this._translationLocale
+		}
+		set translationLocale(e) {
+			this._translationLocale = e
+		}
+	}
+	var c = function(e, t, n, o) {
+		return new(n || (n = Promise))((function(s, a) {
+			function i(e) {
+				try {
+					r(o.next(e))
+				} catch (t) {
+					a(t)
+				}
+			}
+
+			function l(e) {
+				try {
+					r(o.throw(e))
+				} catch (t) {
+					a(t)
+				}
+			}
+
+			function r(e) {
+				var t;
+				e.done ? s(e.value) : (t = e.value, t instanceof n ? t : new n((function(e) {
+					e(t)
+				}))).then(i, l)
+			}
+			r((o = o.apply(e, t || [])).next())
+		}))
+	};
+	n(77321);
+	let d = null;
+
+	function u(e) {
+		const t = document.getElementById("content");
+		t.className = "text-center", t.innerHTML = "";
+		const n = document.createElement("h1"),
+			o = document.createElement("p");
+		if (n.textContent = d.t("youSuccessfullyLoggedIn"), o.textContent = "web" == e ? o.textContent = d.t("thisWindowWillCloseIn5Seconds") : d.t("youMayCloseThisWindow"), n.className = "font-weight-semibold", o.className = "mb-4", t.appendChild(n), t.appendChild(o), "web" == e) {
+			const e = document.createElement("button");
+			e.textContent = d.t("close"), e.className = "bg-primary text-white border-0 rounded py-2 px-3", e.addEventListener("click", (() => {
+				window.close()
+			})), t.appendChild(e);
+			let n = Number(o.textContent.match(/\d+/)[0]);
+			const s = setInterval((() => {
+				n > 1 ? (o.textContent = o.textContent.replace(String(n), String(n - 1)), n--) : (clearInterval(s), window.close())
+			}), 1e3)
+		}
+	}
+	window.addEventListener("load", (() => c(void 0, void 0, void 0, (function*() {
+		const e = o("duoFramelessUrl");
+		if (e) return void
+		function(e) {
+			const t = new URL(e);
+			if ("https:" !== t.protocol || !t.hostname.endsWith(".duosecurity.com") && !t.hostname.endsWith(".duofederal.com")) throw new Error("Invalid redirect URL");
+			window.location.href = decodeURIComponent(e)
+		}(e);
+		const t = o("client"),
+			n = o("code"),
+			s = o("state");
+		if (d = new r(navigator.language, "locales"), yield d.init(), "web" === t) {
+			const e = new BroadcastChannel("duoResult");
+			e.postMessage({
+				code: n,
+				state: s
+			}), e.close(), u(t)
+		} else "browser" === t ? (window.postMessage({
+			command: "duoResult",
+			code: n,
+			state: s
+		}, "*"), u(t)) : "mobile" !== t && "desktop" !== t || ("desktop" === t && u(t), document.location.replace("bitwarden://duo-callback?code=" + encodeURIComponent(n) + "&state=" + encodeURIComponent(s)))
+	}))))
+})();
 //# sourceMappingURL=duo-redirect.a8aee212b2ba62d2b905.js.map
--- web-vault/connectors/webauthn.04cf1b411806d417a4e0.js
+++ web-vault/connectors/webauthn.04cf1b411806d417a4e0.js
@@ -1,2 +1,151 @@
-(()=>{"use strict";var e={39684:(e,t,n)=>{n.r(t)}},t={};function n(r){var o=t[r];if(void 0!==o)return o.exports;var a=t[r]={exports:{}};return e[r](a,a.exports,n),a.exports}function r(e){const t=window.location.href;e=e.replace(/[\[\]]/g,"\\$&");const n=new RegExp("[?&]"+e+"(=([^&#]*)|&|#|$)").exec(t);return n?n[2]?decodeURIComponent(n[2].replace(/\+/g," ")):"":null}function o(e,t=!1){return t&&(e=e.replace(/ /g,"+")),decodeURIComponent(Array.prototype.map.call(atob(e),(e=>"%"+("00"+e.charCodeAt(0).toString(16)).slice(-2))).join(""))}function a(e){if(Array.isArray(e)&&(e=Uint8Array.from(e)),e instanceof ArrayBuffer&&(e=new Uint8Array(e)),e instanceof Uint8Array){let t="";const n=e.byteLength;for(let r=0;r<n;r++)t+=String.fromCharCode(e[r]);e=window.btoa(t)}if("string"!=typeof e)throw new Error("could not coerce to string");return e=e.replace(/\+/g,"-").replace(/\//g,"_").replace(/=*$/g,"")}n.r=e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n(39684);const i="bitwarden://webauthn-callback";let c,d=!1,l=null,u=null,s=null,p=null,f=null,g=!1,y=!1,b=!1,m=null;function h(){if(d)return;if(p=r("parent"),!p)return void x("No parent.");p=decodeURIComponent(p),f=new URL(p).origin;"1"===r("v")?function(){const e=r("data");if(!e)return void x("No data.");c=o(e),l=r("headerText"),u=r("btnText"),s=r("btnReturnText")}():function(){let e=null;try{e=JSON.parse(o(r("data")))}catch(t){return void x("Cannot parse data.")}g=null!=e.callbackUri||!0===e.mobile,c=e.data,l=e.headerText,u=e.btnText,s=e.btnReturnText}(),d=!0}function w(){if(b=!1,"credentials"in navigator)if(h(),c){try{m=function(e){const t=JSON.parse(e),n=t.challenge.replace(/-/g,"+").replace(/_/g,"/");return t.challenge=Uint8Array.from(atob(n),(e=>e.charCodeAt(0))),t.allowCredentials.forEach((e=>{const t=e.id.replace(/\_/g,"/").replace(/\-/g,"+");e.id=Uint8Array.from(atob(t),(e=>e.charCodeAt(0)))})),t}(c)}catch(e){return void x("Cannot parse webauthn data.")}y=!1,g||-1!==navigator.userAgent.indexOf(" Safari/")&&-1===navigator.userAgent.indexOf("Chrome")||U()}else x("No data.");else x("WebAuthn is not supported in this browser.")}function U(){y||navigator.credentials.get({publicKey:m}).then(A).catch(x)}function x(e){g?(document.location.replace(i+"?error="+encodeURIComponent(e)),v(i+"?error="+encodeURIComponent(e))):parent.postMessage("error|"+e,p)}function A(e){if(b)return;const t=function(e){const t=e.response,n=new Uint8Array(t.authenticatorData),r=new Uint8Array(t.clientDataJSON),o=new Uint8Array(e.rawId),i=new Uint8Array(t.signature),c={id:e.id,rawId:a(o),type:e.type,extensions:e.getClientExtensionResults(),response:{authenticatorData:a(n),clientDataJson:a(r),signature:a(i)}};return JSON.stringify(c)}(e);g?(document.location.replace(i+"?data="+encodeURIComponent(t)),v(i+"?data="+encodeURIComponent(t))):(parent.postMessage("success|"+t,p),b=!0)}function v(e){const t=document.getElementById("webauthn-button");t.innerText=decodeURI(s),t.onclick=()=>{document.location.replace(e)}}document.addEventListener("DOMContentLoaded",(()=>{var e;if(w(),window.addEventListener("message",(e=>{e.origin&&""!==e.origin&&e.origin===f&&("stop"===e.data?y=!0:"start"===e.data&&y&&w())}),!1),e="ready",g||parent.postMessage("info|"+e,p),h(),l){document.getElementById("webauthn-header").innerText=decodeURI(l)}if(u){const e=document.getElementById("webauthn-button");e.innerText=decodeURI(u),e.onclick=U}}))})();
+(() => {
+	"use strict";
+	var e = {
+			39684: (e, t, n) => {
+				n.r(t)
+			}
+		},
+		t = {};
+
+	function n(r) {
+		var o = t[r];
+		if (void 0 !== o) return o.exports;
+		var a = t[r] = {
+			exports: {}
+		};
+		return e[r](a, a.exports, n), a.exports
+	}
+
+	function r(e) {
+		const t = window.location.href;
+		e = e.replace(/[\[\]]/g, "\\$&");
+		const n = new RegExp("[?&]" + e + "(=([^&#]*)|&|#|$)").exec(t);
+		return n ? n[2] ? decodeURIComponent(n[2].replace(/\+/g, " ")) : "" : null
+	}
+
+	function o(e, t = !1) {
+		return t && (e = e.replace(/ /g, "+")), decodeURIComponent(Array.prototype.map.call(atob(e), (e => "%" + ("00" + e.charCodeAt(0).toString(16)).slice(-2))).join(""))
+	}
+
+	function a(e) {
+		if (Array.isArray(e) && (e = Uint8Array.from(e)), e instanceof ArrayBuffer && (e = new Uint8Array(e)), e instanceof Uint8Array) {
+			let t = "";
+			const n = e.byteLength;
+			for (let r = 0; r < n; r++) t += String.fromCharCode(e[r]);
+			e = window.btoa(t)
+		}
+		if ("string" != typeof e) throw new Error("could not coerce to string");
+		return e = e.replace(/\+/g, "-").replace(/\//g, "_").replace(/=*$/g, "")
+	}
+	n.r = e => {
+		"undefined" != typeof Symbol && Symbol.toStringTag && Object.defineProperty(e, Symbol.toStringTag, {
+			value: "Module"
+		}), Object.defineProperty(e, "__esModule", {
+			value: !0
+		})
+	}, n(39684);
+	const i = "bitwarden://webauthn-callback";
+	let c, d = !1,
+		l = null,
+		u = null,
+		s = null,
+		p = null,
+		f = null,
+		g = !1,
+		y = !1,
+		b = !1,
+		m = null;
+
+	function h() {
+		if (d) return;
+		if (p = r("parent"), !p) return void x("No parent.");
+		p = decodeURIComponent(p), f = new URL(p).origin;
+		"1" === r("v") ? function() {
+			const e = r("data");
+			if (!e) return void x("No data.");
+			c = o(e), l = r("headerText"), u = r("btnText"), s = r("btnReturnText")
+		}() : function() {
+			let e = null;
+			try {
+				e = JSON.parse(o(r("data")))
+			} catch (t) {
+				return void x("Cannot parse data.")
+			}
+			g = null != e.callbackUri || !0 === e.mobile, c = e.data, l = e.headerText, u = e.btnText, s = e.btnReturnText
+		}(), d = !0
+	}
+
+	function w() {
+		if (b = !1, "credentials" in navigator)
+			if (h(), c) {
+				try {
+					m = function(e) {
+						const t = JSON.parse(e),
+							n = t.challenge.replace(/-/g, "+").replace(/_/g, "/");
+						return t.challenge = Uint8Array.from(atob(n), (e => e.charCodeAt(0))), t.allowCredentials.forEach((e => {
+							const t = e.id.replace(/\_/g, "/").replace(/\-/g, "+");
+							e.id = Uint8Array.from(atob(t), (e => e.charCodeAt(0)))
+						})), t
+					}(c)
+				} catch (e) {
+					return void x("Cannot parse webauthn data.")
+				}
+				y = !1, g || -1 !== navigator.userAgent.indexOf(" Safari/") && -1 === navigator.userAgent.indexOf("Chrome") || U()
+			} else x("No data.");
+		else x("WebAuthn is not supported in this browser.")
+	}
+
+	function U() {
+		y || navigator.credentials.get({
+			publicKey: m
+		}).then(A).catch(x)
+	}
+
+	function x(e) {
+		g ? (document.location.replace(i + "?error=" + encodeURIComponent(e)), v(i + "?error=" + encodeURIComponent(e))) : parent.postMessage("error|" + e, p)
+	}
+
+	function A(e) {
+		if (b) return;
+		const t = function(e) {
+			const t = e.response,
+				n = new Uint8Array(t.authenticatorData),
+				r = new Uint8Array(t.clientDataJSON),
+				o = new Uint8Array(e.rawId),
+				i = new Uint8Array(t.signature),
+				c = {
+					id: e.id,
+					rawId: a(o),
+					type: e.type,
+					extensions: e.getClientExtensionResults(),
+					response: {
+						authenticatorData: a(n),
+						clientDataJson: a(r),
+						signature: a(i)
+					}
+				};
+			return JSON.stringify(c)
+		}(e);
+		g ? (document.location.replace(i + "?data=" + encodeURIComponent(t)), v(i + "?data=" + encodeURIComponent(t))) : (parent.postMessage("success|" + t, p), b = !0)
+	}
+
+	function v(e) {
+		const t = document.getElementById("webauthn-button");
+		t.innerText = decodeURI(s), t.onclick = () => {
+			document.location.replace(e)
+		}
+	}
+	document.addEventListener("DOMContentLoaded", (() => {
+		var e;
+		if (w(), window.addEventListener("message", (e => {
+				e.origin && "" !== e.origin && e.origin === f && ("stop" === e.data ? y = !0 : "start" === e.data && y && w())
+			}), !1), e = "ready", g || parent.postMessage("info|" + e, p), h(), l) {
+			document.getElementById("webauthn-header").innerText = decodeURI(l)
+		}
+		if (u) {
+			const e = document.getElementById("webauthn-button");
+			e.innerText = decodeURI(u), e.onclick = U
+		}
+	}))
+})();
 //# sourceMappingURL=webauthn.04cf1b411806d417a4e0.js.map
